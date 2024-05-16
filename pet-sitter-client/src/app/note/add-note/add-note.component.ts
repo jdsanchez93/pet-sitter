60bd32995eac4cf9438636746d55d182ab911472
jdsanchez93 <jdsanchez2011@gmail.com>
2024-05-16 10:30:56 -0600

    basic image upload component

--- pet-sitter-client/src/app/note/add-note/add-note.component.ts
+++ pet-sitter-client/src/app/note/add-note/add-note.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { AbstractControlOptions, FormBuilder, FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { NoteService } from '../note.service';
-import { Note } from '../../../models';
+import { ImageUploadComponent } from '../../photo/image-upload/image-upload.component';
 
 @Component({
   selector: 'app-add-note',
   standalone: true,
-  imports: [MatInputModule, ReactiveFormsModule, MatButtonModule],
+  imports: [MatInputModule, ReactiveFormsModule, MatButtonModule, ImageUploadComponent],
   templateUrl: './add-note.component.html',
   styleUrl: './add-note.component.scss'
 })
@@ -17,11 +17,6 @@ export class AddNoteComponent {
     title: new FormControl('', { nonNullable: true, validators: Validators.required }),
     description: new FormControl('', { nonNullable: true, validators: Validators.required }),
   });
-
-  // noteForm = this.fb.nonNullable.group({
-  //   title: '',
-  //   description: '',
-  // }, { validators: [Validators.required] });
 
   constructor(private noteService: NoteService, private fb: FormBuilder) { }
 
---
+++ pet-sitter-client/src/app/photo/image-upload/image-upload.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-image-upload',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './image-upload.component.html',
+  styleUrl: './image-upload.component.scss'
+})
+export class ImageUploadComponent {
+  fileToUpload: File | null = null;
+  preview: any;
+
+  handleFileInput(event: Event) {
+    let fileList: FileList | null = (event.currentTarget as HTMLInputElement).files;
+    if (fileList && fileList.length > 0) {
+      this.fileToUpload = fileList[0];
+
+      var reader: FileReader = new FileReader();
+
+      reader.onloadend = (e) => {
+        this.preview = reader.result as string;
+     };
+
+     reader.readAsDataURL(this.fileToUpload);
+    }
+  }
+}
