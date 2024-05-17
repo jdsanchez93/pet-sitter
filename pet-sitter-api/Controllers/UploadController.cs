0ad4730823ebcd331a9cb8bdc7703301613d4a2f
jd <jdsanchez2011@gmail.com>
2024-05-17 14:16:25 -0600

    add backend for upload

---
+++ pet-sitter-api/Controllers/UploadController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pet_sitter_api.Entities;
+
+namespace pet_sitter_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UploadController : ControllerBase
+{
+    private readonly ILogger<UploadController> _logger;
+    private readonly IConfiguration _configuration;
+    private readonly HttpClient _httpClient;
+
+    public UploadController(ILogger<UploadController> logger, IConfiguration configuration, HttpClient httpClient)
+    {
+        _logger = logger;
+        _configuration = configuration;
+        _httpClient = httpClient;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] UploadData d)
+    {
+        try
+        {
+            var url = _configuration["AWS:ApiGatewayUrl"];
+            var httpResponse = await _httpClient.PostAsJsonAsync(url, d);
+
+            var apiGatewayResponse = await httpResponse.Content.ReadFromJsonAsync<ApiGatewayResponse>();
+            return Created("/api/Upload", apiGatewayResponse);
+        }
+        catch (System.Exception e)
+        {
+            _logger.LogError(e, "Post");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+
+    public class UploadData
+    {
+        public int ItemId { get; set; }
+        public string? ItemType { get; set; }
+        public string? Extension { get; set; }
+    }
+
+    public class ApiGatewayResponse
+    {
+        public string? S3ObjectName { get; set; }
+        public string? UploadUrl { get; set; }
+    }
+}
