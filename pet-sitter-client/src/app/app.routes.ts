291bbcb81e07d4107a7c11659ee3182f31de82d2
jd <jdsanchez2011@gmail.com>
2024-07-03 18:44:15 -0600

    wip: edit note using existing component

--- pet-sitter-client/src/app/app.routes.ts
+++ pet-sitter-client/src/app/app.routes.ts
@@ -4,4 +4,5 @@ export const routes: Routes = [
     { path: 'notes', loadComponent: () => import('./note/note-list/note-list.component').then(mod => mod.NoteListComponent) },
     { path: 'note/:id', loadComponent: () => import('./note/single-note/single-note.component').then(mod => mod.SingleNoteComponent) },
     { path: 'new-note', loadComponent: () => import('./note/add-note/add-note.component').then(mod => mod.AddNoteComponent) },
+    { path: 'edit-note/:id', loadComponent: () => import('./note/add-note/add-note.component').then(mod => mod.AddNoteComponent) },
 ];
--- pet-sitter-client/src/app/note/add-note/add-note.component.ts
+++ pet-sitter-client/src/app/note/add-note/add-note.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -8,7 +8,7 @@ import { MatCardModule } from '@angular/material/card';
 import { switchMap, tap } from 'rxjs';
 import { Note } from '../../../models';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-note',
@@ -17,7 +17,7 @@ import { Router, RouterModule } from '@angular/router';
   styleUrl: './add-note.component.scss',
   imports: [MatInputModule, ReactiveFormsModule, MatButtonModule, ImageUploadComponent, MatCardModule, MatProgressSpinnerModule, RouterModule]
 })
-export class AddNoteComponent {
+export class AddNoteComponent implements OnInit {
   noteForm = this.fb.nonNullable.group({
     title: new FormControl('', { nonNullable: true, validators: Validators.required }),
     description: new FormControl('', { nonNullable: true, validators: Validators.required }),
@@ -25,21 +25,58 @@ export class AddNoteComponent {
 
   isLoading: boolean = false;
 
+  @Input({ required: false }) note: Note | undefined;
+
   @ViewChild(ImageUploadComponent) imageUploadComponent!: ImageUploadComponent;
 
-  constructor(private noteService: NoteService, private fb: FormBuilder, private router: Router) { }
+  constructor(private noteService: NoteService, private fb: FormBuilder, private router: Router, private route: ActivatedRoute) { }
+  
+  ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        switchMap((params: ParamMap) => {
+          let noteId = Number(params.get('id'));
+          return this.noteService.getNote(noteId);
+        }),
+        tap(x => this.note = x)
+      )
+      .subscribe()
+
+  }
 
   onSubmit() {
+    if (this.isUpdatingExistingNote()) {
+      console.log('updatenote')
+      // this.updateNote();
+    } else {
+      console.log('createnote')
+      // this.createNote();
+    }
+  }
+
+  private isUpdatingExistingNote(): boolean {
+    console.log('isUpdatingExistingNote', this.note?.noteId);
+    if (this.note?.noteId) {
+      return true;
+    }
+    return false;
+  }
+
+  private createNote() {
     this.isLoading = true;
     let n: Partial<Note> = this.noteForm.value;
     this.imageUploadComponent.uploadPhoto().pipe(
       switchMap(({ s3ObjectName }) => {
         if (s3ObjectName) {
-          n.photos = [{ photoId: 0, isDelete: false, s3Key: s3ObjectName }]
+          n.photos = [{ photoId: 0, isDelete: false, s3Key: s3ObjectName }];
         }
-        return this.noteService.createNote(n)
+        return this.noteService.createNote(n);
       }),
-      tap(note => this.router.navigate(['/note', note.noteId ]))
+      tap(note => this.router.navigate(['/note', note.noteId]))
     ).subscribe();
+  }
+
+  private updateNote() {
+    this.noteService.patchNote(this.note?.noteId!, this.noteForm.value);
   }
 }
