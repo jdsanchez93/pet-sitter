f63f754ac9254034c39a878aeba86bb1d2552abf
jdsanchez93 <jdsanchez2011@gmail.com>
2024-08-23 11:38:00 -0600

    route to edit note from note card

--- pet-sitter-client/src/app/note/note-card/note-card.component.ts
+++ pet-sitter-client/src/app/note/note-card/note-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Note } from '../../../models';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-note-card',
@@ -13,5 +14,11 @@ import { MatButtonModule } from '@angular/material/button';
 export class NoteCardComponent {
 
   @Input({ required: true }) note!: Note
+
+  constructor(private router: Router) { }
+
+  editNote() {
+    this.router.navigate(['/edit-note', this.note.noteId])
+  }
 
 }
